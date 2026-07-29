999cf530adaa5e0abe3d619f64efdab1599f84e7
Rutik Patil <rp4372022@gmail.com>
2026-07-29 18:54:14 +0530

    Array syntax

---
+++ Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] resolution = {144,360,489,720,1080};
+
+        int currentRes = resolution[4];
+
+        System.out.println("Current resolution is : "+currentRes);
+    }
+}
